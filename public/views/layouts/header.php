93aaa9054a42ceb98dc219e7aac2d2892ecb0224
Brandon Guerra <104736190+BrGE09@users.noreply.github.com>
2023-05-19 21:22:28 -0600

    bootstrap

--- public/views/layouts/header.php
+++ public/views/layouts/header.php
@@ -4,4 +4,5 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="<?= $url ?>/resources/CSS/styles.css">
+<link rel="stylesheet" href="<?= $url ?>/resources/BOOTSTRAP/js/bootstrap.bundle.min.js"">
 <title>CODIM</title>
