5574b41ddaedb3713ef7c4773988e089e8009523
Brandon Guerra <104736190+BrGE09@users.noreply.github.com>
2023-05-19 21:17:15 -0600

    Estructura

--- index.php
+++ index.php
@@ -2,11 +2,7 @@
 <html lang="es">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/resources/CSS/styles.css">
-    <title>CODIM</title>
+    <?php require_once("public/views/layouts/header.php") ?>
 </head>
 
 <body class="bg">
---
+++ public/views/layouts/header.php
@@ -0,0 +1,7 @@
+<?php $url = "/CODIM"; ?>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="<?= $url ?>/resources/CSS/styles.css">
+<title>CODIM</title>
